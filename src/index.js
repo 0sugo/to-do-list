ed068ef29d9ecc3abda26e103b13c7bb581af60f
0sugo <josugo26@gmail.com>
2023-03-29 23:13:23 +0300

    Add arraydynamically to the html page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
     const element = document.createElement('div');
   
 //loadash imported by script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['', ''], ' ');
     element.classList.add('hello');
 
   
@@ -12,3 +12,57 @@ function component() {
   }
   
   document.body.appendChild(component());
+
+  const tasks = [
+    {
+      description: 'visit park',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Take a walk',
+      completed: false,
+      index: 2,
+    },
+    {
+      description: 'complete project 1',
+      completed: false,
+      index: 3,
+    },
+  ];
+  
+  function renderTaskList() {
+    const taskList = document.getElementById('task-list');
+    taskList.innerHTML = '';
+    tasks.forEach((task) => {
+      const listItem = document.createElement('li');
+  
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.checked = task.completed;
+      checkbox.addEventListener('change', (event) => {
+        task.completed = event.target.checked;
+        // eslint-disable-next-line no-use-before-define
+        checkAllCompleted();
+      });
+      listItem.appendChild(checkbox);
+  
+      const description = document.createElement('span');
+      description.innerText = task.description;
+      if (task.completed) {
+        description.classList.add('completed');
+      }
+      listItem.appendChild(description);
+  
+      taskList.appendChild(listItem);
+    });
+  }
+  
+  function checkAllCompleted() {
+    const allCompleted = tasks.every((task) => task.completed);
+    const clearAllButton = document.getElementById('clear-all');
+    clearAllButton.disabled = !allCompleted;
+  }
+  
+  renderTaskList();
+  checkAllCompleted();
