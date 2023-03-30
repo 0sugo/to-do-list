33d34e4f74bbbbe1083203cfbb1f15a15b04ab75
0sugo <josugo26@gmail.com>
2023-03-30 15:55:49 +0300

    Add assigning indexes to objects

--- src/index.js
+++ src/index.js
@@ -10,29 +10,57 @@ function component() {
 }
 
 document.body.appendChild(component());
-
+let index = 0;
 const tasks = [
-  {
-    description: 'visit park',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Take a walk',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'complete project 1',
-    completed: false,
-    index: 3,
-  },
+//   {
+//     description: 'visit park',
+//     completed: false,
+//     index: 1,
+//   },
+//   {
+//     description: 'Take a walk',
+//     completed: false,
+//     index: 2,
+//   },
+//   {
+//     description: 'complete project 1',
+//     completed: false,
+//     index: 3,
+//   },
 ];
 
+// fetch information from form
+
+
+// fetch store && to array
+function attacher(){
+    let completed = false;
+    let newObj = {
+      description: 'kwera',
+      completed: false,
+      index: index,
+    }
+    let newObj2 = {
+        description: 'kwera',
+        completed: false,
+        index: index,
+      }
+    
+    tasks.push(newObj);
+    tasks.push(newObj2);
+    
+
+    // const adder = document.getElementById('adder');
+    
+}
+
+//rendering info to html
 function renderTaskList() {
   const taskList = document.getElementById('task-list');
   taskList.innerHTML = '';
-  tasks.forEach((task) => {
+
+  tasks.forEach((task,index) => {
+    task.index = index;
     const listItem = document.createElement('li');
 
     const checkbox = document.createElement('input');
@@ -54,13 +82,18 @@ function renderTaskList() {
 
     taskList.appendChild(listItem);
   });
+  console.log(tasks);
+  index+=index;
 }
 
+// Check if all tasks are completed to activate button
 function checkAllCompleted() {
   const allCompleted = tasks.every((task) => task.completed);
   const clearAllButton = document.getElementById('clear-all');
   clearAllButton.disabled = !allCompleted;
 }
 
+
+attacher();
 renderTaskList();
 checkAllCompleted();
