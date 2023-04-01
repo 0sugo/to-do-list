34eaa19109a0a3f611caf92aeb0d98e8c01f3bd1
0sugo <josugo26@gmail.com>
2023-04-01 13:26:13 +0300

    Add Reassigning indexes functionality

--- src/addRemove.js
+++ src/addRemove.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 import './style.css';
-import { updateTaskStatus  }  from './interactive.js';
-import { clearAllButton } from './interactive.js';
+import { updateTaskStatus, clearAllButton } from './interactive.js';
 
 export const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 const taskList = document.getElementById('task-list');
@@ -27,26 +26,35 @@ function checkAllCompleted() {
       localStorage.setItem('tasks', JSON.stringify(tasks));
       checkAllCompleted();
       taskList.innerHTML = '';
-      clearAllButton();
+      // clearAllButton();
     }
   });
 }
-
 
 // render infor to html
 export default function renderTaskList() {
   taskList.innerHTML = '';
 
   tasks.forEach((task, index) => {
-    task.index = index;
+    // task.index = index;
     const listItem = document.createElement('li');
     listItem.id = 'identifier';
 
-      // add a draggable attribute to the li element
-      listItem.draggable = true;
+    // function to re assign indexes
+    function assignIndexesToLocalTasks() {
+      const tasks = JSON.parse(localStorage.getItem('tasks'));
 
-     // set up the dragstart event listener to store the task index in the dataTransfer object
-     listItem.addEventListener('dragstart', (event) => {
+      tasks.forEach((task, index) => {
+        task.index= index;
+      });
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    // add a draggable attribute to the li element
+    listItem.draggable = true;
+
+    // set up the dragstart event listener to store the task index in the dataTransfer object
+    listItem.addEventListener('dragstart', (event) => {
       event.dataTransfer.setData('text/plain', index);
       event.dataTransfer.effectAllowed = 'move';
     });
@@ -67,18 +75,19 @@ export default function renderTaskList() {
         tasks[sourceIndex] = targetTask;
         tasks[index] = sourceTask;
         localStorage.setItem('tasks', JSON.stringify(tasks));
+        assignIndexesToLocalTasks();
         renderTaskList();
       }
     });
 
-      // update the indexes of the list items
-  const listItems = document.querySelectorAll('.task');
-  listItems.forEach((listItem, newIndex) => {
+    // update the indexes of the list items
+    const listItems = document.querySelectorAll('.task');
+    listItems.forEach((listItem, newIndex) => {
     // retrieve the index from the list item's id attribute
-    const taskIndex = parseInt(listItem.id.split('-')[1]);
-    listItem.dataset.index = taskIndex;
-  });
-
+      const taskIndex = parseInt(listItem.id.split('-')[1]);
+      listItem.dataset.index = taskIndex;
+      renderTaskList();
+    });
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
@@ -113,100 +122,90 @@ export default function renderTaskList() {
     editInput.style.display = 'none';
     listItem.appendChild(editInput);
 
-
     listItem.addEventListener('click', (event) => {
       if (event.target.id === 'edit') {
         description.style.display = 'none';
         editInput.style.display = 'inline-block';
         editInput.focus();
-        listItem.style.backgroundColor='#326789';
+        listItem.style.backgroundColor = '#326789';
         svg.style.display = 'none';
-
       }
     });
 
     editInput.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    const newDescription = editInput.value.trim();
-    if (newDescription !== '') {
-      task.description = newDescription;
-      description.innerText = newDescription;
-      description.style.display = 'inline-block';
-      editInput.style.display = 'none';
-      svgDelete.style.display = 'none';
-      listItem.style.backgroundColor='unset';
-      svg.style.display = 'unset';
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-  }
-});
+      if (event.key === 'Enter') {
+        const newDescription = editInput.value.trim();
+        if (newDescription !== '') {
+          task.description = newDescription;
+          description.innerText = newDescription;
+          description.style.display = 'inline-block';
+          editInput.style.display = 'none';
+          svgDelete.style.display = 'none';
+          listItem.style.backgroundColor = 'unset';
+          svg.style.display = 'unset';
+          localStorage.setItem('tasks', JSON.stringify(tasks));
+        }
+      }
+    });
 
+    listItem.appendChild(editInput);
 
-listItem.appendChild(editInput);
+    // create a new SVG element
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.id = 'savg';
+    svg.setAttribute('style', 'cursor: default;'); // set default cursor
+    svg.addEventListener('mouseover', () => {
+      svg.style.cursor = 'move'; // set move cursor on hover
+    });
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    svg.setAttribute('fill', 'none');
+    svg.setAttribute('viewBox', '0 0 24 24');
+    svg.setAttribute('stroke-width', '1.5');
+    svg.setAttribute('stroke', 'currentColor');
+    svg.setAttribute('class', 'w-6 h-6');
+    svg.setAttribute('width', '18');
+    svg.setAttribute('height', '32');
 
-        // create a new SVG element
-        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.id = 'savg';
-        svg.setAttribute("style", "cursor: default;"); // set default cursor
-svg.addEventListener("mouseover", () => {
-  svg.style.cursor = "move"; // set move cursor on hover
-});
-        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        svg.setAttribute("fill", "none");
-        svg.setAttribute("viewBox", "0 0 24 24");
-        svg.setAttribute("stroke-width", "1.5");
-        svg.setAttribute("stroke", "currentColor");
-        svg.setAttribute("class", "w-6 h-6");
-        svg.setAttribute("width", "18");
-        svg.setAttribute("height", "32");
+    // create a new path element
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('stroke-linecap', 'round');
+    path.setAttribute('stroke-linejoin', 'round');
+    path.setAttribute('d', 'M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z');
 
-        // create a new path element
-        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute("stroke-linecap", "round");
-        path.setAttribute("stroke-linejoin", "round");
-        path.setAttribute("d", "M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z");
+    // add the path element to the SVG element
+    svg.appendChild(path);
+    listItem.appendChild(svg);
 
-        // add the path element to the SVG element
-        svg.appendChild(path);
-        listItem.appendChild(svg);
+    // add event listener to SVG element
+    description.addEventListener('click', () => {
+      editButton.click();
+      svgDelete.style.display = 'block';
+    });
 
-        // add event listener to SVG element
-        description.addEventListener('click', () => {
-          editButton.click();
-          svgDelete.style.display = 'block';
-        });
+    // add event listener to edit button
+    editButton.addEventListener('click', () => {
+      description.style.display = 'none';
+      editInput.style.display = 'inline-block';
+      editInput.focus();
+    });
 
-        // add event listener to edit button
-        editButton.addEventListener('click', () => {
-          description.style.display = 'none';
-          editInput.style.display = 'inline-block';
-          editInput.focus();
-        });
+    const svgDelete = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svgDelete.style.display = 'none';
+    svgDelete.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    svgDelete.setAttribute('fill', 'none');
+    svgDelete.setAttribute('viewBox', '0 0 24 24');
+    svgDelete.setAttribute('stroke-width', '1.5');
+    svgDelete.setAttribute('stroke', 'currentColor');
+    svgDelete.setAttribute('class', 'w-6 h-6 svgDelete'); // add a new class name "svgDelete"
+    svgDelete.setAttribute('width', '18');
+    svg.setAttribute('height', '32');
 
-        const svgDelete = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svgDelete.style.display = 'none';
-        svgDelete.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        svgDelete.setAttribute("fill", "none");
-        svgDelete.setAttribute("viewBox", "0 0 24 24");
-        svgDelete.setAttribute("stroke-width", "1.5");
-        svgDelete.setAttribute("stroke", "currentColor");
-        svgDelete.setAttribute("class", "w-6 h-6 svgDelete"); // add a new class name "svgDelete"
-        svgDelete.setAttribute("width", "18");
-        svg.setAttribute("height", "32");
-
-        const pathDelete = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        pathDelete.setAttribute("stroke-linecap", "round");
-        pathDelete.setAttribute("stroke-linejoin", "round");
-        pathDelete.setAttribute("d", "M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0");
-        svgDelete.appendChild(pathDelete);  
-        listItem.appendChild(svgDelete);  
-
-
-
-
-    // const deleteButton = document.createElement('button');
-    // deleteButton.innerText = 'Delete';
-    // deleteButton.id = 'delete';
+    const pathDelete = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    pathDelete.setAttribute('stroke-linecap', 'round');
+    pathDelete.setAttribute('stroke-linejoin', 'round');
+    pathDelete.setAttribute('d', 'M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0');
+    svgDelete.appendChild(pathDelete);
+    listItem.appendChild(svgDelete);
 
     svgDelete.addEventListener('click', () => {
       const index = tasks.findIndex((t) => t.index === task.index);
@@ -216,7 +215,6 @@ svg.addEventListener("mouseover", () => {
         localStorage.setItem('tasks', JSON.stringify(tasks));
       }
     });
-    // listItem.appendChild(deleteButton);
 
     taskList.appendChild(listItem);
   });
@@ -238,7 +236,7 @@ myForm.addEventListener('submit', (event) => {
     const newObj = {
       description: individualTask,
       completed: false,
-      index: tasks.length,
+      // index: tasks.length,
     };
     tasks.push(newObj);
     localStorage.setItem('tasks', JSON.stringify(tasks));
--- src/interactive.js
+++ src/interactive.js
@@ -1,14 +1,12 @@
-import {tasks} from './addRemove'
-import {renderTaskList} from './addRemove'
+import { tasks, renderTaskList } from './addRemove';
 
 export function updateTaskStatus(task, status) {
-    task.completed = status;
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
+  task.completed = status;
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
 export function clearAllChecked() {
-    const tasks = JSON.parse(localStorage.getItem("tasks"));
-    const uncheckedTasks = tasks.filter(task => !task.checked);
-    localStorage.setItem("tasks", JSON.stringify(uncheckedTasks));
-    renderTasks();
-  }
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  const uncheckedTasks = tasks.filter((task) => !task.checked);
+  localStorage.setItem('tasks', JSON.stringify(uncheckedTasks));
+}
